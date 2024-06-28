098a734d2d10db8194ccc831a70d9b9182ff2a84
Rhys P. Judd <rhyspjudd.dev@gmail.com>
2024-06-28 17:50:59 +0100

    Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,25 @@
 import { motion } from "framer-motion";
+import './styles.scss'
 
-const Button = () => {
+interface ButtonProps {
+  buttonText: string;
+  onClick: () => void;
+ }
+
+const Button: React.FC<ButtonProps> = ({ buttonText, onClick }) => {
   return ( 
-  <>
+  <a className="button">
     <motion.button
-      whileTap={{ scale: 18.9 }}
-      whileHover={{
-        scale: 18.2,
-        transition: { duration: 1 },
-      }}
-    >Click
+      onClick={onClick}
+      whileTap={{ scale: .95 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      // whileHover={{
+      //   scale: 1.1,
+      //   transition: { duration: 0.2 },
+      // }}
+    >{buttonText}
     </motion.button>
-  </> 
+  </a> 
   );
 }
  
